672be53074983e9abb63d0140235d7fbe8772a53
Y.Umemura <yuga0211ume@gmail.com>
2024-05-21 00:20:16 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
-      <body>
+      <main>
         <Game />
-      </body>
+      </main>
     </div>
   )
 }
